a082e382990059687822c4210a577590abe23667
yurimaurino <ymaurinoborges@gmail.com>
2024-09-03 09:46:48 -0300

    starting the project admin template

---
+++ admin-template/src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className={`
+        flex justify-center items-center h-screen
+        bg-gradient-to-r from-green-500 to-blue-500  
+      `}>Admin</h1>
+    </div>    
+  );
+}
