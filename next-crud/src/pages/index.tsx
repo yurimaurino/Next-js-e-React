5dddcee41f303c36288a7eac74c9e06072007483
yurimaurino <ymaurinoborges@gmail.com>
2024-09-12 14:35:33 -0300

    table component

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,6 +1,24 @@
 import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 22, '3'),
+    new Cliente('Pedro', 54, '4'),
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log('Excluir...')
+  }
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -8,7 +26,10 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples ">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}>
+        </Tabela>
       </Layout>
     </div>
   );
